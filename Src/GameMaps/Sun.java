db4198bb71433257eaa24f59aeea0d1814a033dd
muhammadkasyfil <141574422+muhammadkasyfil@users.noreply.github.com>
2024-05-16 20:28:16 +0700

    Update Sun.java

--- Src/GameMaps/Sun.java
+++ Src/GameMaps/Sun.java
@@ -3,25 +3,47 @@ package Src.GameMaps;
 import java.util.Random;
 
 public class Sun {
-    private int sunWaitingTime;
+    // private long sunWaitingTime;
     private static int lastSun;
     private int sunPerProduction = 25;
-
-    public static void SunProducing(int sunWaitingTime, int sunPerProduction){
-        Random rand = new Random();
-
-        sunWaitingTime =  rand.nextInt(6) + 5;
-
-        try {
-            Thread.sleep(sunWaitingTime * 1000);
-        } catch (InterruptedException ssunn){
-            ssunn.printStackTrace();
+    private boolean running = true;
+    
+    public void addSun(int sun){
+        synchronized (this){
+            lastSun += sun;
+            System.out.println("Sun ditambah: " + sun + ", Total sun: " + lastSun);
         }
-        
-        lastSun = lastSun + sunPerProduction;
     }
 
-    public long getSun(){
+    public boolean spendSun(int sun) {
+        synchronized (this){
+            if (lastSun >= sun){
+                lastSun -= sun;
+                System.out.println("Sun yang digunakan: " + sun + ", Sisa sun: " + lastSun);
+                return true;
+            } else {
+                System.out.println("Sun tidak cukup. Diperlukan: " + sun + ", Sun yang tersedia: " + lastSun);
+                return false;
+            }
+        }
+    }
+
+    public void run(){
+        try {
+            while (running) {
+                Thread.sleep((long) (Math.random() * (10000 - 5000) + 5000));
+                addSun(25);
+            }
+        } catch (InterruptedException e) {
+                System.out.println("Sun error");
+        }
+    }
+
+    public void sunerror() {
+        running = false;
+    }
+
+    public int getSun(){
         return lastSun;
     }
 }
